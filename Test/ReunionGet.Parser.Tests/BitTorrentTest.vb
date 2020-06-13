89b44aa8c1d268b5f51b728bbb430cf97f1bceef
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-06-14 01:43:45 +0800

    Add test for sample folder torrent.

--- Test/ReunionGet.Parser.Tests/BitTorrentTest.vb
+++ Test/ReunionGet.Parser.Tests/BitTorrentTest.vb
@@ -48,4 +48,26 @@ Public Class BitTorrentTest
             }, o)
         End Using
     End Sub
+
+    <Fact>
+    Public Sub TestFolderTorrent()
+        Using resourceStream = GetType(BitTorrentTest).Assembly.GetManifestResourceStream(GetType(BitTorrentTest), "Sample Folder.torrent")
+            Assert.NotNull(resourceStream)
+            Dim torrent = BitTorrent.FromStream(resourceStream)
+
+            Assert.Equal("udp://tracker.publicbt.com:80/announce", torrent.Announce.AbsoluteUri)
+            Assert.False(torrent.IsSingleFile)
+            Assert.Null(torrent.SingleFileLength)
+            Assert.Equal(32768, torrent.PieceLength)
+            Assert.Equal("Sample Folder", torrent.Name)
+            Assert.Equal((38L, "Nested\Sample2.txt"), torrent.Files(0))
+            Assert.Equal((46L, "Sample1.txt"), torrent.Files(1))
+            Assert.Equal(1, torrent.PieceHashes.Length)
+            Assert.Equal("C6449B60ED556C148F0A50F359479F2C1AA9249E", torrent.PieceHashes(0).ToString())
+            Assert.Equal(84L, torrent.TotalLength)
+            Assert.False(torrent.IsPrivate)
+            Assert.Equal(#2020/6/13 17:29:37#, torrent.CreationTime.Value.UtcDateTime)
+            Assert.Equal("C98B4C1D23A1A93C18F93B19104DEE5F8E0F9B56", torrent.InfoHash.ToString())
+        End Using
+    End Sub
 End Class
