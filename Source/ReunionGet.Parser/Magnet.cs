09f0f98c6956bbfe9027a27fab4de54bbded091b
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-06-15 16:42:06 +0800

    Parse more fields for magnet.

--- Source/ReunionGet.Parser/Magnet.cs
+++ Source/ReunionGet.Parser/Magnet.cs
@@ -16,6 +16,22 @@ namespace ReunionGet.Parser
 
         public string? OriginalString { get; }
 
+        public string? DisplayName { get; }
+
+        public IReadOnlyCollection<Uri> Trackers { get; }
+
+        #region Uncommon Fields
+        public long? ExactLength { get; }
+
+        public Uri? ExactSource { get; }
+
+        public IReadOnlyCollection<Uri> AcceptableSources { get; }
+
+        public IReadOnlyCollection<string> KeywordTopic { get; }
+
+        public Uri? ManifestTopic { get; }
+        #endregion
+
         public Magnet(string source)
             : this(new Uri(source))
         {
@@ -23,22 +39,62 @@ namespace ReunionGet.Parser
 
         public Magnet(Uri uri)
         {
-            if (!TryGetMagnetParts(uri, out var hashAlgorithm, out byte[]? hash))
+            if (!TryGetMagnetParts(uri,
+                out var hashAlgorithm,
+                out byte[]? hash,
+                out string? displayName,
+                out Uri[]? trackers,
+                out long? exactLength,
+                out Uri? exactSource,
+                out Uri[]? acceptableSources,
+                out string[]? keywordTopic,
+                out Uri? manifestTopic))
                 throw new MagnetFormatException();
 
             HashAlgorithm = hashAlgorithm;
             HashValue = new HashBlock(hash);
             OriginalString = uri.OriginalString;
+            DisplayName = displayName;
+            Trackers = trackers ?? Array.Empty<Uri>();
+            ExactLength = exactLength;
+            ExactSource = exactSource;
+            AcceptableSources = acceptableSources ?? Array.Empty<Uri>();
+            KeywordTopic = keywordTopic ?? Array.Empty<string>();
+            ManifestTopic = manifestTopic;
         }
 
-        private Magnet(MagnetHashAlgorithm hashAlgorithm, byte[] hash, string originalString)
+        private Magnet(MagnetHashAlgorithm hashAlgorithm,
+            byte[] hash,
+            string originalString,
+            string? displayName,
+            Uri[]? trackers,
+            long? exactLength,
+            Uri? exactSource,
+            Uri[]? acceptableSources,
+            string[]? keywordTopic,
+            Uri? manifestTopic)
         {
             HashAlgorithm = hashAlgorithm;
             HashValue = new HashBlock(hash);
             OriginalString = originalString;
+            DisplayName = displayName;
+            Trackers = trackers ?? Array.Empty<Uri>();
+            ExactLength = exactLength;
+            ExactSource = exactSource;
+            AcceptableSources = acceptableSources ?? Array.Empty<Uri>();
+            KeywordTopic = keywordTopic ?? Array.Empty<string>();
+            ManifestTopic = manifestTopic;
         }
 
-        public Magnet(MagnetHashAlgorithm hashAlgorithm, HashBlock hash)
+        public Magnet(MagnetHashAlgorithm hashAlgorithm,
+            HashBlock hash,
+            string? displayName = null,
+            IReadOnlyCollection<Uri>? trackers = null,
+            long? exactLength = null,
+            Uri? exactSource = null,
+            IReadOnlyCollection<Uri>? acceptableSources = null,
+            IReadOnlyCollection<string>? keywordTopic = null,
+            Uri? manifestTopic = null)
         {
             int expectedHashSize = hashAlgorithm switch
             {
@@ -53,6 +109,13 @@ namespace ReunionGet.Parser
 
             HashAlgorithm = hashAlgorithm;
             HashValue = hash;
+            DisplayName = displayName;
+            Trackers = trackers ?? Array.Empty<Uri>();
+            ExactLength = exactLength;
+            ExactSource = exactSource;
+            AcceptableSources = acceptableSources ?? Array.Empty<Uri>();
+            KeywordTopic = keywordTopic ?? Array.Empty<string>();
+            ManifestTopic = manifestTopic;
         }
 
         public static bool TryCreate(string source, [NotNullWhen(true)] out Magnet? magnet)
@@ -64,9 +127,27 @@ namespace ReunionGet.Parser
 
         public static bool TryCreate(Uri uri, [NotNullWhen(true)] out Magnet? magnet)
         {
-            if (TryGetMagnetParts(uri, out var hashAlgorithm, out byte[]? hash))
+            if (TryGetMagnetParts(uri,
+                out var hashAlgorithm,
+                out byte[]? hash,
+                out string? displayName,
+                out Uri[]? trackers,
+                out long? exactLength,
+                out Uri? exactSource,
+                out Uri[]? acceptableSources,
+                out string[]? keywordTopic,
+                out Uri? manifestTopic))
             {
-                magnet = new Magnet(hashAlgorithm, hash, uri.OriginalString);
+                magnet = new Magnet(hashAlgorithm,
+                                    hash,
+                                    uri.OriginalString,
+                                    displayName,
+                                    trackers,
+                                    exactLength,
+                                    exactSource,
+                                    acceptableSources,
+                                    keywordTopic,
+                                    manifestTopic);
                 return true;
             }
             else
@@ -76,10 +157,27 @@ namespace ReunionGet.Parser
             }
         }
 
-        private static bool TryGetMagnetParts(Uri uri, out MagnetHashAlgorithm hashAlgorithm, [NotNullWhen(true)] out byte[]? hash)
+        private static bool TryGetMagnetParts(
+            Uri uri,
+            out MagnetHashAlgorithm hashAlgorithm,
+            [NotNullWhen(true)] out byte[]? hash,
+            out string? displayName,
+            out Uri[]? trackers,
+            out long? exactLength,
+            out Uri? exactSource,
+            out Uri[]? acceptableSources,
+            out string[]? keywordTopic,
+            out Uri? manifestTopic)
         {
             hashAlgorithm = default;
             hash = null;
+            displayName = null;
+            trackers = null;
+            exactLength = null;
+            exactSource = null;
+            acceptableSources = null;
+            keywordTopic = null;
+            manifestTopic = null;
 
             if (!uri.IsAbsoluteUri)
                 return false;
@@ -92,10 +190,7 @@ namespace ReunionGet.Parser
 
             var query = HttpUtility.ParseQueryString(uri.Query);
 
-            string? xt = query["xt"];
-            if (xt is null)
-                return false;
-
+            if (query["xt"] is string xt)
             {
                 string[]? parts = xt.Split(':');
                 if (parts.Length != 3 || parts[0] != "urn") // TODO: use collection pattern
@@ -135,6 +230,51 @@ namespace ReunionGet.Parser
                     default:
                         return false;
                 }
+            }
+            else
+            {
+                return false;
+            }
+
+            displayName = query["dn"];
+
+            if (query.GetValues("tr") is string[] tr)
+            {
+                trackers = new Uri[tr.Length];
+                for (int i = 0; i < tr.Length; i++)
+                    if (!Uri.TryCreate(tr[i], UriKind.Absolute, out trackers[i]!))
+                        return false;
+            }
+
+            if (query["xl"] is string xl)
+            {
+                if (long.TryParse(xl, out long xlValue))
+                    exactLength = xlValue;
+                else
+                    return false;
+            }
+
+            if (query["kt"] is string kt)
+                keywordTopic = kt.Split('+', StringSplitOptions.RemoveEmptyEntries);
+
+            if (query["xs"] is string xs)
+            {
+                if (!Uri.TryCreate(xs, UriKind.Absolute, out exactSource))
+                    return false;
+            }
+
+            if (query.GetValues("as") is string[] @as)
+            {
+                acceptableSources = new Uri[@as.Length];
+                for (int i = 0; i < @as.Length; i++)
+                    if (!Uri.TryCreate(@as[i], UriKind.Absolute, out acceptableSources[i]!))
+                        return false;
+            }
+
+            if (query["mt"] is string mt)
+            {
+                if (!Uri.TryCreate(mt, UriKind.Absolute, out manifestTopic))
+                    return false;
             }
 
             return true;
