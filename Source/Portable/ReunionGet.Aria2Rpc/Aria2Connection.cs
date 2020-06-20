cbb31c1b2c90726f38ae8ffabba554be7ef7bef9
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-06-20 23:13:18 +0800

    Basic abstraction of json rpc.

---
+++ Source/Portable/ReunionGet.Aria2Rpc/Aria2Connection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ReunionGet.Aria2Rpc.Json;
+
+namespace ReunionGet.Aria2Rpc
+{
+    public sealed class Aria2Connection : IDisposable
+    {
+        private static readonly JsonSerializerOptions s_serializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters =
+            {
+                new ValueTupleToArrayConverter()
+            }
+        };
+
+        private readonly HttpClient _httpClient;
+        private readonly string _tokenParam;
+
+        public void Dispose() => _httpClient.Dispose();
+
+        public Aria2Connection(Uri baseUri, string token)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = baseUri
+            };
+            _tokenParam = "token:" + token;
+        }
+
+        public Aria2Connection(string address, int port, string token)
+            : this(new Uri($"http://{address}:{port}"), token)
+        {
+        }
+
+        private async Task<TResponse> DoRpcAsync<TRequest, TResponse>(string method, TRequest @params)
+        {
+            var rpcRequest = new JsonRpcRequest<TRequest>(method, @params);
+            var httpResponse = await _httpClient.PostAsJsonAsync("jsonrpc", rpcRequest, s_serializerOptions)
+                .ConfigureAwait(false);
+
+            var rpcResponse = await httpResponse.Content.ReadFromJsonAsync<JsonRpcResponse<TResponse>>(s_serializerOptions)
+                .ConfigureAwait(false);
+
+            return rpcResponse.CheckSuccessfulResult();
+        }
+
+        private async Task DoRpcAsync<TRequest>(string method, TRequest @params)
+        {
+            var rpcRequest = new JsonRpcRequest<TRequest>(method, @params);
+            var httpResponse = await _httpClient.PostAsJsonAsync("jsonrpc", rpcRequest, s_serializerOptions)
+                .ConfigureAwait(false);
+
+            var rpcResponse = await httpResponse.Content.ReadFromJsonAsync<JsonRpcResponse<object>>(s_serializerOptions)
+                .ConfigureAwait(false);
+
+            rpcResponse.CheckSuccess();
+        }
+    }
+}
---
+++ Source/Portable/ReunionGet.Aria2Rpc/Json/JsonRpcMessage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ReunionGet.Aria2Rpc.Json
+{
+    internal abstract class JsonRpcMessage
+    {
+        public string Jsonrpc { get; set; } = "2.0";
+
+        public string Id { get; set; } = "NONEED";
+    }
+
+    internal class JsonRpcRequest<T> : JsonRpcMessage
+    {
+        public string Method { get; set; }
+
+        public T Params { get; set; }
+
+        public JsonRpcRequest(string method, T @params)
+        {
+            Method = method;
+            Params = @params;
+        }
+    }
+
+    internal class JsonRpcResponse<T> : JsonRpcMessage
+    {
+        // TODO: use T??
+
+        [MaybeNull, AllowNull]
+        public T Result { get; set; } = default;
+
+        public JsonRpcError? Error { get; set; }
+
+        [return: NotNull]
+        public T CheckSuccessfulResult()
+        {
+            CheckSuccess();
+
+            return Result
+                ?? throw new InvalidOperationException("The response object hasn't been correctly deserialized, or the remote endpoint doesn't follow the standard.");
+        }
+
+        public void CheckSuccess()
+        {
+            if (Error != null)
+                throw new JsonRpcException(Error);
+        }
+    }
+
+    internal class JsonRpcError
+    {
+        public int Code { get; set; }
+
+        public string? Message { get; set; }
+    }
+
+    public class JsonRpcException : InvalidOperationException
+    {
+        public int Code { get; }
+
+        internal JsonRpcException(JsonRpcError errorObj)
+            : base(errorObj.Message ?? $"A json RPC operation failed with error code {errorObj.Code}.")
+            => Code = errorObj.Code;
+    }
+}
