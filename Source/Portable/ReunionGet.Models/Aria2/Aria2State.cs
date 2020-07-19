31edbe64b6582b04992001e72d813a647cbcaac1
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-07-19 19:45:42 +0800

    First prototype of aria2 state model.

---
+++ Source/Portable/ReunionGet.Models/Aria2/Aria2State.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReunionGet.Aria2Rpc.Json;
+using ReunionGet.Aria2Rpc.Json.Responses;
+
+namespace ReunionGet.Models.Aria2
+{
+    public class Aria2State
+    {
+        private readonly Aria2Host _host;
+        public Aria2State(Aria2Host host) => _host = host;
+
+        private readonly Dictionary<Aria2GID, Aria2Task> _tasksDict
+            = new Dictionary<Aria2GID, Aria2Task>();
+
+        public IReadOnlyCollection<Aria2GID> TrackedGIDs => _tasksDict.Keys;
+        public IReadOnlyCollection<Aria2Task> AllTasks => _tasksDict.Values;
+        public IEnumerable<Aria2Task> TopLevelTasks => _tasksDict.Values.Where(x => x.IsTopLevel);
+
+        internal void PostAllTrackedRefresh(IEnumerable<DownloadProgressStatus> status)
+        {
+            foreach (var s in status)
+            {
+                var task = _tasksDict[s.Gid];
+                task.Load(s);
+                foreach (var followedGID in s.FollowedBy ?? Enumerable.Empty<Aria2GID>())
+                {
+                    if (!_tasksDict.ContainsKey(followedGID))
+                    {
+                        var followedTask = new Aria2Task(_host, followedGID, task);
+                        _tasksDict.Add(followedGID, followedTask);
+                        AnyTrackedTaskAdded?.Invoke(followedTask);
+                        task.AddFollowedTask(followedTask);
+                    }
+                }
+            }
+        }
+
+        public event Action<Aria2Task>? TopLevelTaskAdded;
+        public event Action<Aria2Task>? AnyTrackedTaskAdded;
+
+        public async Task<Aria2Task> AddMangetTaskAsync(string magnet)
+        {
+            var gid = await _host.Connection.AddUriAsync(magnet).ConfigureAwait(false);
+            var task = new Aria2Task(_host, gid);
+            _tasksDict.Add(gid, task);
+            TopLevelTaskAdded?.Invoke(task);
+            AnyTrackedTaskAdded?.Invoke(task);
+            return task;
+        }
+    }
+}
---
+++ Source/Portable/ReunionGet.Models/Aria2/Aria2Task.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using ReunionGet.Aria2Rpc.Json;
+using ReunionGet.Aria2Rpc.Json.Responses;
+
+namespace ReunionGet.Models.Aria2
+{
+    public class Aria2Task
+    {
+        private readonly Aria2Host _host;
+
+        public Aria2Task(Aria2Host host, Aria2GID gid, Aria2Task? following = null)
+        {
+            _host = host;
+            GID = gid;
+            Following = following;
+        }
+
+        public Aria2GID GID { get; }
+
+        // TODO: use MemberNotNullWhen
+        public bool Loaded => RpcResponse != null;
+
+        public DownloadProgressStatus? RpcResponse { get; private set; }
+
+        public bool IsTopLevel => Following is null;
+
+        public Aria2Task? Following { get; }
+
+        private List<Aria2Task>? _followedTasks;
+        public IReadOnlyList<Aria2Task> FollowedTasks
+            => _followedTasks
+            ?? (IReadOnlyList<Aria2Task>)Array.Empty<Aria2Task>();
+
+        internal void Load(DownloadProgressStatus rpcResponse)
+        {
+            RpcResponse = rpcResponse;
+            StatusLoaded?.Invoke();
+        }
+
+        internal void AddFollowedTask(Aria2Task followedTask)
+        {
+            _followedTasks ??= new List<Aria2Task>();
+            _followedTasks.Add(followedTask);
+            FollowedTaskAdded?.Invoke(followedTask);
+        }
+
+        public event Action? StatusLoaded;
+
+        public event Action<Aria2Task>? FollowedTaskAdded;
+    }
+}
